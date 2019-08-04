85028013a48e6f86c3c23ca11dd5152b91061e8b
Ivan Duka <ivan.duka.pm@gmail.com>
2019-08-04 17:32:45 -0600

    fixed bug with button

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './Button.module.css';
 
 const button = ({ children, clicked, btnType }) => (
-  <button className={[classes.Button, classes[btnType].join(' ')]} onClick={clicked}>
+  <button className={[classes.Button, classes[btnType]].join(' ')} onClick={clicked}>
     {children}
   </button>
 );
