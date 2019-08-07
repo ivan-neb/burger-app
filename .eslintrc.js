7b27ce1b49d010bd9f2a2e90a25c42d5a3ae8174
Ivan Duka <ivan.duka.pm@gmail.com>
2019-08-06 18:56:23 -0600

    fixed integration with prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  extends: ['airbnb'],
+  extends: ['airbnb', 'prettier'],
   parser: 'babel-eslint',
   globals: {
     Atomics: 'readonly',
