f03514a1672fcc7a4a52838c8370ced02f31d940
Ivan Duka <ivan.duka.pm@gmail.com>
2019-08-11 11:53:24 -0600

    converted order summary to functional component

--- src/components/Burger/OrderSummary/OrderSummary.jsx
+++ src/components/Burger/OrderSummary/OrderSummary.jsx
@@ -2,45 +2,33 @@ import React from 'react';
 import Aux from '../../../hoc/Auxiliary/Auxiliary';
 import Button from '../../UI/Button/Button';
 import convertToDec from '../../../util/convertToDec';
-import PropTypes from 'prop-types';
 
-class OrderSummary extends React.Component {
-  // This can be converted to a functional component
+const OrderSummary = props => {
+  const { ingredients, purchaseCancel, purchaseContinue, totalPrice } = props;
 
-  render = () => {
-    const { ingredients, purchaseCancel, purchaseContinue, totalPrice } = this.props;
+  const ingredientSummary = Object.keys(ingredients).map(key => (
+    <li key={key}>
+      <span>{key.toLocaleUpperCase()}</span>: {ingredients[key]}
+    </li>
+  ));
 
-    const ingredientSummary = Object.keys(ingredients).map(key => (
-      <li key={key}>
-        <span>{key.toLocaleUpperCase()}</span>: {ingredients[key]}
-      </li>
-    ));
-
-    return (
-      <Aux>
-        <h3>Your order:</h3>
-        <p>A delicious burger with the following ingredients:</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total totalPrice: {convertToDec(totalPrice)}</strong>
-        </p>
-        <p>Continue to checkout?</p>
-        <Button clicked={purchaseCancel} btnType='Danger'>
-          CANCEL
-        </Button>
-        <Button clicked={purchaseContinue} btnType='Success'>
-          CONTINUE
-        </Button>
-      </Aux>
-    );
-  };
-}
-
-OrderSummary.propTypes = {
-  ingredients: PropTypes.object,
-  purchaseCancel: PropTypes.func,
-  purchaseContinue: PropTypes.func,
-  totalPrice: PropTypes.number,
+  return (
+    <Aux>
+      <h3>Your order:</h3>
+      <p>A delicious burger with the following ingredients:</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total totalPrice: {convertToDec(totalPrice)}</strong>
+      </p>
+      <p>Continue to checkout?</p>
+      <Button clicked={purchaseCancel} btnType='Danger'>
+        CANCEL
+      </Button>
+      <Button clicked={purchaseContinue} btnType='Success'>
+        CONTINUE
+      </Button>
+    </Aux>
+  );
 };
 
 export default OrderSummary;
