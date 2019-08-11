1bf9316eee1531132ea95193d8a8929ac2d7dcb7
Ivan Duka <ivan.duka.pm@gmail.com>
2019-08-11 15:39:51 -0600

    fetching orders

--- src/components/Order/Order.jsx
+++ src/components/Order/Order.jsx
@@ -2,14 +2,49 @@ import React from 'react';
 
 import classes from './Order.module.css';
 
+const objToString = obj => {
+  if (typeof obj === 'string') {
+    return obj;
+  }
+
+  const array = [];
+  // eslint-disable-next-line no-restricted-syntax
+  for (const key in obj) {
+    let value = obj[key];
+    if (typeof value === 'object' && value !== null) {
+      value = objToString(value);
+    }
+    array.push(`${key}: ${value}`);
+  }
+  return array.join(', ');
+};
+
 const order = props => {
-  const { ingredients, price } = props;
+  const { data } = props;
+  const { customer, deliveryMethod, id, price, ingredients } = data;
+
+  const ingredientsString = objToString(ingredients);
+  const customerString = objToString(customer);
 
   return (
     <div className={classes.Order}>
-      <p>Ingredients: </p>
       <p>
-        Price: <strong></strong>
+        <strong>ID: </strong>
+        {id}
+      </p>
+      <p>
+        <strong>Customer: </strong>
+        {customerString}
+      </p>
+      <p>
+        <strong>Delivery Method: </strong>
+        {deliveryMethod}
+      </p>
+      <p>
+        <strong>Ingredients</strong>: {ingredientsString}
+      </p>
+      <p>
+        Price: <strong>{price}</strong>
       </p>
     </div>
   );
--- src/containers/Orders/Orders.jsx
+++ src/containers/Orders/Orders.jsx
@@ -1,16 +1,39 @@
 import React from 'react';
 
 import Order from '../../components/Order/Order';
+import axios from '../../axios-orders';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 class Orders extends React.Component {
-  state = {};
+  state = { orders: [], loading: true };
+
+  componentDidMount() {
+    axios
+      .get('/orders.json')
+      .then(res => {
+        const fetchedOrdersArray = [];
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const key in res.data) {
+          const order = {
+            ...res.data[key],
+            id: key,
+          };
+          fetchedOrdersArray.push(order);
+        }
+
+        this.setState({ orders: fetchedOrdersArray, loading: false });
+      })
+      .catch(() => {
+        this.setState({ loading: false });
+      });
+  }
 
   render() {
     const { orders } = this.state;
-    // const ordersElements = orders.map(x => x);
-
-    return <div>{null}</div>;
+    const orderElements = orders.map(order => <Order data={order} key={order.id} />);
+    return <div>{orderElements}</div>;
   }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
