7c2192b1e9fd7fbebb120a9724b8d06b71829ab9
Ivan Duka <ivan.duka.pm@gmail.com>
2019-08-12 18:47:23 -0600

    fixed label association with input element

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,5 +17,10 @@ module.exports = {
     sourceType: 'module',
   },
   plugins: ['react'],
-  rules: { 'react/prop-types': [0], 'react/jsx-one-expression-per-line': [0], 'guard-for-in': [0] },
+  rules: {
+    'react/prop-types': [0],
+    'react/jsx-one-expression-per-line': [0],
+    'guard-for-in': [0],
+    'jsx-a11y/label-has-for': [0],
+  },
 };
--- src/components/UI/Input/Input.jsx
+++ src/components/UI/Input/Input.jsx
@@ -5,16 +5,19 @@ import classes from './Input.module.css';
 const Input = props => {
   const { label, inputtype } = props;
 
-  const defaultInput = <input className={classes.InputElement} {...props} />;
+  const randomID = Math.random().toString();
+  const defaultInput = <input className={classes.InputElement} id={randomID} {...props} />;
   const types = {
     input: defaultInput,
-    textarea: <textarea className={classes.InputElement} {...props} />,
+    textarea: <textarea className={classes.InputElement} id={randomID} {...props} />,
   };
   const inputElement = types[inputtype] || defaultInput;
 
   return (
     <div className={classes.Input}>
-      <label className={classes.Label}>{label}</label>
+      <label className={classes.Label} htmlFor={randomID}>
+        {label}
+      </label>
       {inputElement}
     </div>
   );
